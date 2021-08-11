215381723eb1182e019b56cdc3ba46e702576b8c
서예준 <skgy322@gmail.com>
2021-08-11 13:29:22 +0900

    learning_python2

--- hello_world.py
+++ hello_world.py
@@ -135,6 +135,64 @@
 #a = "hello world"
 #print(a.split(" "), type(a.split(" ")))
 
-data = "     1234     "
-print(data.strip(),"end")
+#data = "     1234     "
+#print(data.strip(),"end")
 
+#movie_rank = ['닥터 스트레인지','스플릿','럭키']
+#movie_rank.append("배트맨")
+#movie_rank.insert(1, "슈퍼맨")
+#del movie_rank[3]
+#del movie_rank[3]
+#print(movie_rank)
+
+#willdel = 4
+#del willdel
+#print(willdel)
+
+#lang1 = ["C","C++","JAVA"]
+#lang2 = ["Python","Go","C#"]
+#langs = lang1 + lang2
+#print(langs)
+
+#nums = [1,2,3,4,5,6,7]
+#print("max: "+str(max(nums)))
+#print("min: "+str(min(nums)))
+
+#nums =[1,2,3,4,5]
+#print(sum(nums))
+
+#cook = ["피자", "김밥", '만두', '양념치킨', '족발', '피자', '김치만두', '쫄면', '쏘세지', '라면', '팥빙수', '김치전', ]
+#print(len(cook))
+
+#nums = [1,2,3,4,5]
+#nums_mean = sum(nums)/len(nums)
+#print(nums_mean)
+
+#price = ['20180728', 100, 130, 140, 150, 160, 170]
+#print(price[1:])
+
+#nums = [1,2,3,4,5,6,7,8,9,10]
+#odd_nums = nums[::2]
+#print(odd_nums)
+
+#nums = [1,2,3,4,5,6,7,8,9,10]
+#even_nums = nums[1::2]
+#print(even_nums)
+
+#nums = [1,2,3,4,5]
+#nums_reverse = nums[::-1]
+#print(nums_reverse)
+
+#interest = ["삼전", "LG전자", "Naver"]
+#print(interest[::2])
+
+#interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+#print("\n".join(interest))
+
+#string = "삼성전자/LG전자/Naver"
+#interest = string.split("/")
+#print(interest, type(interest))
+
+#data = [2,4,3,1,5,10,9]
+#sorted_data = sorted(data)
+#print(sorted_data)
